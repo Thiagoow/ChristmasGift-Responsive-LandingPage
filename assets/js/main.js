007a600e1200bdf779054162c625e170be220234
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-07-06 14:48:05 -0300

    Repos criado 😁😎

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*==================== SHOW MENU ====================*/
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+/*==================== SHOW SCROLL TOP ====================*/
+
+/*==================== DARK LIGHT THEME ====================*/
+
+// Previously selected topic (if user selected)
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+// We validate if the user previously chose a topic
+// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+
+// Activate / deactivate the theme manually with the button
+
+// Add or remove the dark / icon theme
+
+// We save the theme and the current icon that the user chose
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
